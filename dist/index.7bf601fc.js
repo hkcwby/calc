d21fe057ef4b413c86dbe7cc41c8bdcc10b5ee9a
hkcwby <alexanderstevenson@me.com>
2022-11-19 02:49:18 -0600

    building calc functions

--- dist/index.7bf601fc.js
+++ dist/index.7bf601fc.js
@@ -1,9 +1,24 @@
-let currentDisplayValue = 0;
+let currentDisplayValue = "0";
 let storedNumber = null;
 let secondNumber = null;
-console.log(currentDisplayValue);
+let display = document.querySelector("#display-value");
+//main function for clicks
 function onCalcButtonClick(e) {
-    //creating a click animation for the item
+    //run animation
+    animate(e);
+    //run function to determine action
+    entry(e);
+}
+function entry(e) {
+    console.log(e.target.innerHTML);
+    console.log(currentDisplayValue = String(e.target.innerHTML));
+    console.log(currentDisplayValue.concat(e.target.innerHTML));
+    console.log(currentDisplayValue == 0);
+    currentDisplayValue === "0" ? currentDisplayValue = String(e.target.innerHTML) : currentDisplayValue.concat(e.target.innerHTML);
+    display.innerHTML = currentDisplayValue;
+}
+//creating a click animation for the item
+function animate(e) {
     e.target.classList.add("clicked");
     console.log(e.target.classList);
     setTimeout(()=>{
